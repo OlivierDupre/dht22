4410cb1e9f41208e0b864d3f31028c043dfb47e8
Olivier Dupré <dupreolivier@gmail.com>
2019-04-23 23:30:46 +0100

    Added loop with 30seconds frequency

---
+++ dht.py
@@ -0,0 +1,30 @@
+import json
+import time
+from google.cloud import firestore
+from datetime import datetime
+import Adafruit_DHT
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+frequency = config["frequency"] / 1000
+
+# Project ID is determined by the GCLOUD_PROJECT environment variable
+db = firestore.Client()
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+    now = datetime.now()
+    timestamp = ''+now.isoformat()
+
+    print 'Temp ',temperature,' humidity ',humidity
+
+    doc_ref = db.collection(config["gcp"]["firestore"]["collection"]).document(timestamp)
+    doc_ref.set({
+        u'temperature': temperature,
+        u'humidity': humidity,
+        u'date': u''+timestamp
+    })
+
+    time.sleep(frequency)
+
