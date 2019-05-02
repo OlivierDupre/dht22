8a5ceb81b58c11e1c29c6cbae7b05e5f2edad1f2
Olivier Dupré <dupreolivier@gmail.com>
2019-05-03 00:54:38 +0200

    Clean up & factorization of display in a separate function

--- server/index.js
+++ server/index.js
@@ -6,10 +6,6 @@ const db = new Firestore({
     keyFilename: config.gcp.firestore.keyfile
 });
 
-// function wait(milleseconds) {
-//     return new Promise(resolve => setTimeout(resolve, milleseconds))
-// }
-
 function compute(currentRecord, data) {
     return new Promise(resolve => {
         currentTemp = parseFloat(currentRecord.temperature)
@@ -26,6 +22,22 @@ function compute(currentRecord, data) {
         resolve(data);
     });
 };
+
+function display(data, timeFrame) {
+    let averageTemp = data.sum / data.count;
+
+    // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var maxDate = new Date(data.max.date._seconds * 1000);
+
+    // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var minDate = new Date(data.min.date._seconds * 1000);
+
+    console.log('########### ENREGISTREMENT SUR ' + timeFrame + ' ###########' +
+        '\nNombre de températures enregistrées ' + data.count +
+        '\nTempérature moyenne ' + averageTemp +
+        '\nTempérature max ' + parseFloat(data.max.temperature) + ' le ' + maxDate +
+        '\nTempérature minimale ' + parseFloat(data.min.temperature) + ' le ' + minDate);
+}
 
 async function temperature() {
     let collectionRef = db.collection(config.gcp.firestore.collection);
@@ -55,57 +67,15 @@ async function temperature() {
     // Query from most recent to older ones
     collectionRef.where('date', '>', oneMonthAgo).orderBy('date', 'desc').stream().on('data', (record) => {
         currentRecord = record.data();
-        // firstRecord = currentRecord;
-
-        // if (lastRecord === undefined) {
-        //     lastRecord = currentRecord
-        // }
 
         // monthly = await compute(currentRecord, monthly);
         compute(currentRecord, monthly).then(result => { monthly = result });
-        // // console.log('Found document ', record.id);
-        // currentTemp = parseFloat(currentRecord.temperature)
-
-        // if (currentTemp > parseFloat(maxTemp.temperature)) {
-        //     maxTemp = currentRecord
-        // }
-        // if (currentTemp < parseFloat(minTemp.temperature)) {
-        //     minTemp = currentRecord
-        // }
-        // sumTemp += currentTemp;
-        // ++count;
     }).on('end', () => {
-        let averageTemp = monthly.sum / monthly.count;
-
-        // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
-        var maxDate = new Date(monthly.max.date._seconds * 1000);
-
-        // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
-        var minDate = new Date(monthly.min.date._seconds * 1000);
-
-        console.log('Number of temperature recorded ' + monthly.count +
-            '\nAverage temp is ' + averageTemp +
-            '\nMax temp is ' + parseFloat(monthly.max.temperature) + ' on ' + maxDate +
-            '\nMin temp is ' + parseFloat(monthly.min.temperature) + ' on ' + minDate);
-
-        // console.log("First record is ", new Date(firstRecord.date._seconds * 1000));
-        // console.log("Last record is ", new Date(lastRecord.date._seconds * 1000));
+        display(monthly, "UN MOIS");
     });
 
     //     Wed May 01 2019 23:59:33 GMT+0100 (British Summer Time) => { humidity: '47.8%',
     //   temperature: '20.1*',
     //   date: Timestamp { _seconds: 1556751573, _nanoseconds: 749000000 } }
-
-    // collectionRef.get().then((snapshot) => {
-    //     snapshot.forEach((doc) => {
-    //         console.log(doc.id, '=>', doc.data());
-    //         console.log(doc.temperature)
-    //     });
-    // }).catch((err) => {
-    //     console.log('Error getting documents', err);
-    // });
-
-    // await wait(config.frequency)
-    // }
 }
 temperature()
