404a8cdc398514787f5c966bd5eee8ed48e17bca
Olivier Dupré <dupreolivier@gmail.com>
2019-05-03 00:45:16 +0200

    Computing on date base: monthly, weekly, daily, ...

--- server/index.js
+++ server/index.js
@@ -6,79 +6,107 @@ const db = new Firestore({
     keyFilename: config.gcp.firestore.keyfile
 });
 
+// function wait(milleseconds) {
+//     return new Promise(resolve => setTimeout(resolve, milleseconds))
+// }
 
-function wait(milleseconds) {
-    return new Promise(resolve => setTimeout(resolve, milleseconds))
-}
+function compute(currentRecord, data) {
+    return new Promise(resolve => {
+        currentTemp = parseFloat(currentRecord.temperature)
+
+        console.log(currentTemp)
+
+        if (currentTemp > parseFloat(data.max.temperature)) {
+            data.max = currentRecord
+        }
+        if (currentTemp < parseFloat(data.min.temperature)) {
+            data.min = currentRecord
+        }
+        data.sum += currentTemp;
+        data.count++;
+
+        resolve(data);
+    });
+};
 
 async function temperature() {
-    var collectionRef = db.collection(config.gcp.firestore.collection);
+    let collectionRef = db.collection(config.gcp.firestore.collection);
     let firstRecord, lastRecord;
 
-    // while (true) {
-    // collectionRef.orderBy('date').limit(1).get().then((last) => {
-    //     last.forEach((doc) => {
-    //         lastRecord = doc.data();
-    //     });
-    // }).catch((err) => {
-    //     console.log('Error getting documents', err);
-    // });
+    let now = new Date(Date.now());
+    let oneMonthAgo = now,
+        oneWeekAgo = now,
+        oneDayAgo = now,
+        halfDayAgo = now,
+        quarterDayAgo = now,
+        oneHourAgo = now;
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    oneWeekAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    oneDayAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    halfDayAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    quarterDayAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    oneHourAgo.setMonth(oneMonthAgo.getMonth() - 1);
 
-    // collectionRef.orderBy('date', 'desc').limit(1).get().then((first) => {
-    //     first.forEach((doc) => {
-    //         firstRecord = doc.data();
-    //     });
-    // }).catch((err) => {
-    //     console.log('Error getting documents', err);
-    // });
-
-    let count = 0;
-    let sumTemp = 0;
-    let maxTemp = { temperature: -Infinity };
-    let minTemp = { temperature: Infinity };
-
-    let threeDaysAgo = new Date(Date.now());
-    threeDaysAgo.setHours(threeDaysAgo.getHours() - 72);
+    let monthly = { date: oneMonthAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } },
+        weekly = { date: oneWeekAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } },
+        daily = { date: oneDayAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } },
+        halfDay = { date: halfDayAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } },
+        quarterDay = { date: quarterDayAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } },
+        hourly = { date: oneHourAgo, count: 0, sum: 0, max: { temperature: -Infinity }, min: { temperature: Infinity } };
 
     // Query from most recent to older ones
-    collectionRef.where('date', '>', threeDaysAgo).orderBy('date', 'desc').stream().on('data', (record) => {
-        firstRecord = record.data();
+    collectionRef.where('date', '>', oneMonthAgo).orderBy('date', 'desc').stream().on('data', (record) => {
+        currentRecord = record.data();
+        // firstRecord = currentRecord;
 
-        if (lastRecord === undefined) {
-            lastRecord = firstRecord
-        }
+        // if (lastRecord === undefined) {
+        //     lastRecord = currentRecord
+        // }
 
-        // console.log('Found document ', record.id);
-        currentTemp = parseFloat(firstRecord.temperature)
-        if (currentTemp > parseFloat(maxTemp.temperature)) {
-            maxTemp = firstRecord
-        }
-        if (currentTemp < parseFloat(minTemp.temperature)) {
-            minTemp = firstRecord
-        }
-        sumTemp += currentTemp;
-        ++count;
+        // monthly = await compute(currentRecord, monthly);
+        console.log("GOGOGOGO")
+        compute(currentRecord, monthly).then(result => { monthly = result });
+        // // console.log('Found document ', record.id);
+        // currentTemp = parseFloat(currentRecord.temperature)
+
+        // if (currentTemp > parseFloat(maxTemp.temperature)) {
+        //     maxTemp = currentRecord
+        // }
+        // if (currentTemp < parseFloat(minTemp.temperature)) {
+        //     minTemp = currentRecord
+        // }
+        // sumTemp += currentTemp;
+        // ++count;
     }).on('end', () => {
-        let averageTemp = sumTemp / count;
+        let averageTemp = monthly.sum / monthly.count;
 
         // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
-        var maxDate = new Date(maxTemp.date._seconds * 1000);
+        var maxDate = new Date(monthly.max.date._seconds * 1000);
 
         // Create a new JavaScript Date object based on the timestamp multiplied by 1000 so that the argument is in milliseconds, not seconds.
-        var minDate = new Date(minTemp.date._seconds * 1000);
+        var minDate = new Date(monthly.min.date._seconds * 1000);
 
         console.log('Number of temperature recorded ' + count +
             '\nAverage temp is ' + averageTemp +
-            '\nMax temp is ' + parseFloat(maxTemp.temperature) + ' on ' + maxDate +
-            '\nMin temp is ' + parseFloat(minTemp.temperature) + ' on ' + minDate);
+            '\nMax temp is ' + parseFloat(monthly.max.temperature) + ' on ' + maxDate +
+            '\nMin temp is ' + parseFloat(monthly.min.temperature) + ' on ' + minDate);
 
-        console.log("First record is ", new Date(firstRecord.date._seconds * 1000));
-        console.log("Last record is ", new Date(lastRecord.date._seconds * 1000));
+        // console.log("First record is ", new Date(firstRecord.date._seconds * 1000));
+        // console.log("Last record is ", new Date(lastRecord.date._seconds * 1000));
     });
 
     //     Wed May 01 2019 23:59:33 GMT+0100 (British Summer Time) => { humidity: '47.8%',
     //   temperature: '20.1*',
     //   date: Timestamp { _seconds: 1556751573, _nanoseconds: 749000000 } }
+
+    // collectionRef.get().then((snapshot) => {
+    //     snapshot.forEach((doc) => {
+    //         console.log(doc.id, '=>', doc.data());
+    //         console.log(doc.temperature)
+    //     });
+    // }).catch((err) => {
+    //     console.log('Error getting documents', err);
+    // });
 
     // await wait(config.frequency)
     // }
